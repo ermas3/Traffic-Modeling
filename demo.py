39092ae9bcebd64260990441b50986b4ba53bbf2
ermas3 <erik.asgrim@gmail.com>
2025-07-06 17:24:55 +0200

    update demo

--- demo.py
+++ demo.py
@@ -17,4 +17,4 @@ right_overtaking = True
 # Run simulation
 cars = Cars(numCars=numCars, roadLength=roadLength, lanes=lanes, vmax=vmax, sigma=sigma)
 simulation = Simulation(cars)
-simulation.run_animate(propagator=MyPropagator(p=p, right_overtaking=right_overtaking), numsteps=numsteps)
+simulation.run_animate(propagator=Propagator(p=p, right_overtaking=right_overtaking), numsteps=numsteps)
